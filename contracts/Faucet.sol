e77d8dfd4fec386d9b58f83aaeb72bdc9d9ebbb3
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-12 22:04:18 +0000

    Create smart contract that allow to withdraw

---
+++ contracts/Faucet.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract Faucet {
+  address payable public owner;
+
+  constructor() payable {
+    owner = payable(msg.sender);
+  }
+  
+  // Allows users to withdraw a specified amount of ether from the contract
+  function withdraw(uint _amount) payable public {
+    require(_amount <= 100000000000000000);
+    (bool sent, ) = payable(msg.sender).call{value: _amount}("");
+    require(sent, "Failed to send Ether");
+  }
+
+  // Allows the owner to withdraw all of the ether held by the contract
+  function withdrawAll() onlyOwner public {
+    (bool sent, ) = owner.call{value: address(this).balance}("");
+    require(sent, "Failed to send Ether");
+  }
+
+  // Allows the owner to destroy the contract and transfer any remaining ether to their address
+  function destroyFaucet() onlyOwner public {
+    selfdestruct(owner);
+  }
+
+  // Modifier that restricts access to certain functions to the owner of the contract
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+}
